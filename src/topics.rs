0e9f13fd2edc8632cfbeaf28e939732f4ef60a4f
Gibsol <gibsol75@gmail.com>
2023-02-18 13:29:27 +0200

    :construction: A new module with function that returns a vector
    This function returns a vector with 5 different strings in it. They will contain the markdown in the future

---
+++ src/topics.rs
@@ -0,0 +1,13 @@
+// todo: parse the markdown and place the topics in the vector
+
+pub fn topics() -> Vec<String> {
+    let topics = vec![
+        "topic 1".to_string(),
+        "topic 2".to_string(),
+        "topic 3".to_string(),
+        "topic 4".to_string(),
+        "topic 5".to_string(),
+    ];
+
+    topics
+}
